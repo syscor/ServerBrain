62db691e9556242b7325a37d950c61e197f98bc6
Alex Cortes Gasca <yaqueyalex@gmail.com>
2012-11-14 23:45:32 -0600

    base project

---
+++ NeonMika.XML/XMLConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+namespace NeonMika.XML
+{
+    public static class XMLConverter
+    {
+        public static string ConvertXMLPairToXMLString(XMLPair xmlPair)
+        {
+            string xml = "<" + xmlPair.Key + ">";
+
+            if (xmlPair.Value is XMLPair[])
+            {
+                XMLPair[] arr = (XMLPair[])xmlPair.Value;
+                foreach (XMLPair pair in arr)
+                    xml += ConvertXMLPairToXMLString(pair);
+            }
+            else if (xmlPair.Value is XMLPair)
+                xml += ConvertXMLPairToXMLString((XMLPair)xmlPair.Value);
+            else if (xmlPair.Value is Hashtable)
+                xml += ConvertHashtableToXMLString((Hashtable)xmlPair.Value);
+            else
+                xml += xmlPair.Value.ToString();
+
+            xml += "</" + xmlPair.Key + ">";
+
+            return xml;
+
+        }
+
+        public static string ConvertHashtableToXMLString(Hashtable hashtable)
+        {
+            string xml = "";
+
+            foreach (var actualKey in hashtable.Keys)
+            {
+                xml += "<" + actualKey + ">";
+
+                if (hashtable[actualKey] is XMLPair)
+                    xml += ConvertXMLPairToXMLString((XMLPair)hashtable[actualKey]);
+                else if (hashtable[actualKey] is XMLPair[])
+                {
+                    XMLPair[] arr = (XMLPair[])hashtable[actualKey];
+                    foreach (XMLPair pair in arr)
+                        xml += ConvertXMLPairToXMLString(pair);
+                }
+                else if (hashtable[actualKey] is Hashtable)
+                    xml += ConvertHashtableToXMLString((Hashtable)hashtable[actualKey]);
+                else
+                    xml += hashtable[actualKey].ToString();
+
+                xml += "</" + actualKey + ">";
+            }
+
+            return xml;
+        }
+    }
+}
---
+++ NeonMikaWebserver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using System.Collections;
+using NeonMika.Webserver;
+using NeonMika.Webserver.EventArgs;
+using NeonMika.Webserver.Responses;
+
+namespace NeonMikaWebserverExecuteable
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Server WebServer = new Server(PinManagement.OnboardLED);
+            WebServer.AddResponse(new XMLResponse("wave", new XMLResponseMethod(WebserverXMLMethods.Wave)));
+
+            while (true)
+            {
+                PinManagement.OnboardLED.Write(true);
+                Thread.Sleep(100);
+                PinManagement.OnboardLED.Write(false);
+                Thread.Sleep(2400);                
+            }
+        }
+    }
+}
